f0293056c4873c1811b74741fb4ba22da6cd4f91
yhb92 <40283739+yhb92@users.noreply.github.com>
2021-07-03 15:46:41 +0800

    1.1反转二叉树

---
+++ src/leetcode/editor/cn/_226_翻转二叉树.java
@@ -0,0 +1,104 @@
+//翻转一棵二叉树。 
+//
+// 示例： 
+//
+// 输入： 
+//
+//      4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9 
+//
+// 输出： 
+//
+//      4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1 
+//
+// 备注: 
+//这个问题是受到 Max Howell 的 原问题 启发的 ： 
+//
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+// 👍 904 👎 0
+
+package leetcode.editor.cn;
+
+import javax.swing.tree.TreeNode;
+
+public class _226_翻转二叉树 {
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+
+        /**
+         * 思路：
+         *   1.递归遍历数
+         *   2.取到数后将左右子树交换
+         * @param root
+         * @return
+         */
+        public TreeNode invertTree(TreeNode root) {
+            recursion(root);
+            return root;
+        }
+
+        private void recursion(TreeNode node){
+            // 1.终止条件
+            if (node == null) return;
+
+            // 2.交换子树
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            // 3.递归交换下级子树
+            recursion(node.right);
+            recursion(node.left);
+
+        }
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+  
